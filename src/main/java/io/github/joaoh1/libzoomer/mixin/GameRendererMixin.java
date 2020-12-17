3a0f79759ccaa57cc817e725306154d00c8b6b75
joaoh1 <joaoh1@users.noreply.github.com>
2020-12-17 12:17:06 -0300

    Convert tabs to spaces, just for y'all!

--- src/main/java/io/github/joaoh1/libzoomer/mixin/GameRendererMixin.java
+++ src/main/java/io/github/joaoh1/libzoomer/mixin/GameRendererMixin.java
@@ -22,99 +22,99 @@ import io.github.joaoh1.libzoomer.api.ZoomOverlay;
 
 @Mixin(GameRenderer.class)
 public class GameRendererMixin {
-	@Final
-	@Shadow
-	private MinecraftClient client;
-	
-	@Unique
-	private double formerFov;
+    @Final
+    @Shadow
+    private MinecraftClient client;
+    
+    @Unique
+    private double formerFov;
 
-	@Inject(
-		at = @At("HEAD"),
-		method = "tick()V"
-	)
-	private void tickInstances(CallbackInfo info) {
-		for (ZoomInstance instance : ZoomHelper.zoomInstances) {
-			if (instance.getZoomDivisor() == 1.0F) {
-				break;
-			}
+    @Inject(
+        at = @At("HEAD"),
+        method = "tick()V"
+    )
+    private void tickInstances(CallbackInfo info) {
+        for (ZoomInstance instance : ZoomHelper.zoomInstances) {
+            if (instance.getZoomDivisor() == 1.0F) {
+                break;
+            }
 
-			boolean zoom = instance.getZoom();
-			double divisor = 1.0F;
-			if (zoom) {
-				divisor = instance.getZoomDivisor();
-			}
+            boolean zoom = instance.getZoom();
+            double divisor = 1.0F;
+            if (zoom) {
+                divisor = instance.getZoomDivisor();
+            }
 
-			instance.getZoomOverlay().tick(instance.getZoom(), instance.getZoomDivisor(), instance.getTransitionMode().getInternalMultiplier());
-			instance.getTransitionMode().tick(zoom, divisor);
-		}
-	}
+            instance.getZoomOverlay().tick(instance.getZoom(), instance.getZoomDivisor(), instance.getTransitionMode().getInternalMultiplier());
+            instance.getTransitionMode().tick(zoom, divisor);
+        }
+    }
 
-	@Inject(
-		at = @At("RETURN"),
-		method = "getFov(Lnet/minecraft/client/render/Camera;FZ)D",
-		cancellable = true
-	)
-	private double getZoomedFov(Camera camera, float tickDelta, boolean changingFov, CallbackInfoReturnable<Double> cir) {
-		double fov = cir.getReturnValue();
-		boolean zoomedIn = false;
-		double zoomedFov = fov;
-		
-		for (ZoomInstance instance : ZoomHelper.zoomInstances) {
-			boolean instanceIsZoomed = false;
+    @Inject(
+        at = @At("RETURN"),
+        method = "getFov(Lnet/minecraft/client/render/Camera;FZ)D",
+        cancellable = true
+    )
+    private double getZoomedFov(Camera camera, float tickDelta, boolean changingFov, CallbackInfoReturnable<Double> cir) {
+        double fov = cir.getReturnValue();
+        boolean zoomedIn = false;
+        double zoomedFov = fov;
+        
+        for (ZoomInstance instance : ZoomHelper.zoomInstances) {
+            boolean instanceIsZoomed = false;
 
-			if (instance.getTransitionActive()) {
-				instanceIsZoomed = true;
-			}
-			
-			if (instanceIsZoomed) {
-				double divisor = 1.0F;
-				if (instance.getZoom()) {
-					divisor = instance.getZoomDivisor();
-				}
-				zoomedIn = true;
-				zoomedFov = instance.getTransitionMode().applyZoom(zoomedFov, divisor, tickDelta);
-			}
-		}
+            if (instance.getTransitionActive()) {
+                instanceIsZoomed = true;
+            }
+            
+            if (instanceIsZoomed) {
+                double divisor = 1.0F;
+                if (instance.getZoom()) {
+                    divisor = instance.getZoomDivisor();
+                }
+                zoomedIn = true;
+                zoomedFov = instance.getTransitionMode().applyZoom(zoomedFov, divisor, tickDelta);
+            }
+        }
 
-		if (zoomedIn) {
-			cir.setReturnValue(zoomedFov);
-		}
+        if (zoomedIn) {
+            cir.setReturnValue(zoomedFov);
+        }
 
-		if (zoomedFov > formerFov) {
-			if (changingFov) {
-				this.client.worldRenderer.scheduleTerrainUpdate();
-			}
-		}
+        if (zoomedFov > formerFov) {
+            if (changingFov) {
+                this.client.worldRenderer.scheduleTerrainUpdate();
+            }
+        }
 
-		this.formerFov = zoomedFov;
+        this.formerFov = zoomedFov;
 
-		return fov;
-	}
+        return fov;
+    }
 
-	@Inject(
-		at = @At(value = "FIELD", target = "Lnet/minecraft/client/options/GameOptions;hudHidden:Z"),
-		method = "render(FJZ)V"
-	)
-	public void injectZoomOverlay(float tickDelta, long startTime, boolean tick, CallbackInfo info) {
-		if (this.client.options.hudHidden) return;
+    @Inject(
+        at = @At(value = "FIELD", target = "Lnet/minecraft/client/options/GameOptions;hudHidden:Z"),
+        method = "render(FJZ)V"
+    )
+    public void injectZoomOverlay(float tickDelta, long startTime, boolean tick, CallbackInfo info) {
+        if (this.client.options.hudHidden) return;
 
-		RenderSystem.defaultAlphaFunc();
-		RenderSystem.enableBlend();
-		OverlayCancellingHelper.setCancelOverlayRender(false);
-		for (ZoomInstance instance : ZoomHelper.zoomInstances) {
-			ZoomOverlay overlay = instance.getZoomOverlay();
-			overlay.tickBeforeRender();
-			if (overlay.getActive()) {
-				if (overlay.cancelOverlayRendering()) {
-					OverlayCancellingHelper.setCancelOverlayRender(true);
-				}
-				
-				overlay.renderOverlay();	
-			}
-		}
-		RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
-		RenderSystem.enableAlphaTest();
-		RenderSystem.clear(256, MinecraftClient.IS_SYSTEM_MAC);
-	}
+        RenderSystem.defaultAlphaFunc();
+        RenderSystem.enableBlend();
+        OverlayCancellingHelper.setCancelOverlayRender(false);
+        for (ZoomInstance instance : ZoomHelper.zoomInstances) {
+            ZoomOverlay overlay = instance.getZoomOverlay();
+            overlay.tickBeforeRender();
+            if (overlay.getActive()) {
+                if (overlay.cancelOverlayRendering()) {
+                    OverlayCancellingHelper.setCancelOverlayRender(true);
+                }
+                
+                overlay.renderOverlay();	
+            }
+        }
+        RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
+        RenderSystem.enableAlphaTest();
+        RenderSystem.clear(256, MinecraftClient.IS_SYSTEM_MAC);
+    }
 }
