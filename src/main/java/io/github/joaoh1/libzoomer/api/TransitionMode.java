b65d0fa4ed6e6e29ab0f203fea95bb821bcf123f
joaoh1 <joaoh1@users.noreply.github.com>
2021-01-21 15:20:04 -0300

    Javadoc the Dork III: The Dorkening

--- src/main/java/io/github/joaoh1/libzoomer/api/TransitionMode.java
+++ src/main/java/io/github/joaoh1/libzoomer/api/TransitionMode.java
@@ -2,14 +2,42 @@ package io.github.joaoh1.libzoomer.api;
 
 import net.minecraft.util.Identifier;
 
+/**
+ * The transition mode is a sub-instance that handles zooming itself.
+ * It handles how the regular FOV will transition to the zoomed FOV and vice-versa.
+ */
 public interface TransitionMode {
+    /**
+     * Gets the identifier of the transition mode.
+     * @return The transition mode's identifier.
+     */
     Identifier getIdentifier();
 
+    /**
+     * Gets the active state of the transition mode.
+     * @return The transition mode's active state.
+     */
     boolean getActive();
 
+    /**
+     * Applies the zoom to the FOV.
+     * @param fov The original FOV.
+     * @param divisor The zoom divisor.
+     * @param tickDelta The current tick delta.
+     * @return The zoomed FOV.
+     */
     double applyZoom(double fov, double divisor, float tickDelta);
 
+    /**
+     * The tick method. Used in order to keep the internal variables accurate.
+     * @param active The zoom state.
+     * @param divisor The zoom divisor.
+     */
     void tick(boolean active, double divisor);
 
+    /**
+     * Gets the internal multiplier. Used for purposes other than zooming the FOV.
+     * @return The internal multiplier.
+     */
     double getInternalMultiplier();
 }
--- src/main/java/io/github/joaoh1/libzoomer/api/ZoomOverlay.java
+++ src/main/java/io/github/joaoh1/libzoomer/api/ZoomOverlay.java
@@ -3,18 +3,53 @@ package io.github.joaoh1.libzoomer.api;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.util.Identifier;
 
+/**
+ * The zoom overlay is a sub-instance that handles the rendering of an overlay.
+ */
 public interface ZoomOverlay {
+    /**
+     * Gets the identifier of the zoom overlay.
+     * @return The zoom overlay's identifier.
+     */
     Identifier getIdentifier();
 
+    /**
+     * Gets the active state of the zoom overlay.
+     * @return The zoom overlay's active state.
+     */
     boolean getActive();
 
+    /**
+     * Sets the internal Minecraft client.
+     * While not always mandatory, the client is usually used internally in order to help with rendering.
+     * @param newClient The new MinecraftClient instance.
+     * @return The MinecraftClient that has been set.
+     */
     MinecraftClient setClient(MinecraftClient newClient);
 
+    /**
+     * Determines if the zoom overlay should cancel the rendering of anything rendered after that.
+     * By default, it returns false.
+     * @return The state that will be used in order to cancel the rendering or not.
+     */
     default boolean cancelOverlayRendering() { return false; }
 
+    /**
+     * Renders the overlay itself. It's injected by LibZoomer itself.
+     */
     void renderOverlay();
 
+    /**
+     * The tick method. Used in order to keep the internal variables accurate and the overlay functional.
+     * @param active The zoom state.
+     * @param divisor The zoom divisor.
+     * @param transitionMultiplier The transition mode's internal multiplier.
+    */
     void tick(boolean active, double divisor, double transitionMultiplier);
 
+    /**
+     * The tick method used right before the overlay is rendered.
+     * Not required to be implemented.
+     */
     default void tickBeforeRender() {}
 }
