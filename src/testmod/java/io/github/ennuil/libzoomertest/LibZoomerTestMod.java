3efcba35a7586855074c1d9cdae392100ca83d8b
Ennui Langeweile <85590273+EnnuiL@users.noreply.github.com>
2021-06-17 17:39:58 -0300

    Add more comments to the test mod

--- src/testmod/java/io/github/ennuil/libzoomertest/LibZoomerTestMod.java
+++ src/testmod/java/io/github/ennuil/libzoomertest/LibZoomerTestMod.java
@@ -5,6 +5,7 @@ import org.lwjgl.glfw.GLFW;
 import io.github.ennuil.libzoomer.api.ZoomInstance;
 import io.github.ennuil.libzoomer.api.ZoomRegistry;
 import io.github.ennuil.libzoomer.api.modifiers.CinematicCameraMouseModifier;
+import io.github.ennuil.libzoomer.api.modifiers.ContainingMouseModifier;
 import io.github.ennuil.libzoomer.api.modifiers.SpyglassMouseModifier;
 import io.github.ennuil.libzoomer.api.overlays.NoZoomOverlay;
 import io.github.ennuil.libzoomer.api.overlays.SpyglassZoomOverlay;
@@ -30,24 +31,32 @@ public class LibZoomerTestMod implements ModInitializer {
     public void onInitialize() {
         // Michelle. She's an implementation of a very simple zoom key. Tests if there are zoom instance conflicts and spyglass-unrelated things.
         KeyBinding michelle = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.libzoomertest.michelle", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_V, "key.libzoomertest.category"));
+        
+        // Michael's Zoom Instance
         ZoomInstance zoomInstance = ZoomRegistry.registerInstance(new ZoomInstance(
             new Identifier("libzoomertest:zoom"),
             10.0F, new SmoothTransitionMode(),
             new SpyglassMouseModifier(),
             new SpyglassZoomOverlay(new Identifier("libzoomertest:textures/misc/michael.png"))
         ));
+        // Michelle's Zoom Instance
         ZoomInstance electricBoogaloo = ZoomRegistry.registerInstance(new ZoomInstance(
             new Identifier("libzoomertest:zoom2"),
             3.0F, new InstantTransitionMode(),
             new CinematicCameraMouseModifier(),
             new NoZoomOverlay()
         ));
+
+        // This prints out all zoom instances registered so far and some extra info
         for (ZoomInstance instance : ZoomRegistry.getZoomInstances()) {
             System.out.println("Id: " + instance.getInstanceId() + " | Zooming: " + instance.getZoom() + " | Divisor: " + instance.getZoomDivisor());
         }
+        
+        // Register the Michael item
         Registry.register(Registry.ITEM, new Identifier("libzoomertest:michael"), MICHAEL_ITEM);
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            // This is how you get a spyglass-like zoom working
             if (client.player == null) return;
             if (client.options.getPerspective().isFirstPerson() && (client.player.isUsingItem() && client.player.getActiveItem().isOf(MICHAEL_ITEM))) {
                 zoomInstance.setZoom(true);
@@ -55,6 +64,7 @@ public class LibZoomerTestMod implements ModInitializer {
                 zoomInstance.setZoom(false);
             }
 
+            // And this is how you get a simple zoom button working
             electricBoogaloo.setZoom(michelle.isPressed());
         });
     }
